15ef7f4158ab1680529ec7fec6116711febd3caf
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-07 19:37:07 -0500

    Add my router for searching.

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -1,19 +1,55 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const axios = require('axios');
+const convert = require('xml-js');
+
 const router = express.Router();
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-    
+// return all favorite images
+router.get('/keyword/:search', (req, res) => {
+  const search = req.params.search;
+  console.log(search);
+  axios.get(`http://boardgamegeek.com/xmlapi2/search?query=${search}`)
+    .then((response) => {
+      const data = JSON.parse(
+        convert.xml2json(response.data, { compact: true, spaces: 2 })
+      );
+      // console.log(data);
+      const games = data.items;
+      // console.log(games);
+      // If no search results were returned send 404.
+      if (games.item) {
+        console.log('send arr:', games.item);
+        res.send(games.item);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-
+router.get('/game-id/:id', (req, res) => {
+  const search = req.params.id;
+  axios.get(`http://boardgamegeek.com/xmlapi2/thing?id=${search}`)
+    .then((response) => {
+      const data = JSON.parse(
+        convert.xml2json(response.data, { compact: true, spaces: 2 })
+      );
+      const games = data.items;
+      // If no search results were returned send 404.
+      if (games.item) {
+        res.send(games.item);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
