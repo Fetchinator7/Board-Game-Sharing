29ac7bf1aa798e9a9f445e8bead39ca17eaa288c
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-08 19:30:24 -0500

    Experimental code.

--- src/components/App/Nav/Nav.js
+++ src/components/App/Nav/Nav.js
@@ -2,6 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
+// import { TextField } from '@material-ui/core';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
+// import Tabs from '@material-ui/core/Tabs';
+// import Tab from '@material-ui/core/Tab';
+// import Avatar from '@material-ui/core/Avatar';
+// import Fab from '@material-ui/core/Fab';
+// import AddIcon from '@material-ui/icons/Add';
+// import FolderIcon from '@material-ui/icons/Folder';
+// import ListSubheader from '@material-ui/core/ListSubheader';
+// import List from '@material-ui/core/List';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import Collapse from '@material-ui/core/Collapse';
+// import InboxIcon from '@material-ui/icons/MoveToInbox';
+// import DraftsIcon from '@material-ui/icons/Drafts';
+// import SendIcon from '@material-ui/icons/Send';
+// import SettingsIcon from '@material-ui/icons/Settings';
+// import ExpandLess from '@material-ui/icons/ExpandLess';
+// import ExpandMore from '@material-ui/icons/ExpandMore';
+// import StarBorder from '@material-ui/icons/StarBorder';
 import './Nav.css';
 
 const Nav = (props) => (
@@ -22,7 +44,7 @@ const Nav = (props) => (
           <Link className="nav-link" to="/info">
             Info Page
           </Link>
-          <LogOutButton className="nav-link"/>
+          <LogOutButton className="nav-link" />
         </>
       )}
       {/* Always show this link since the about page is not protected */}
@@ -31,7 +53,57 @@ const Nav = (props) => (
       </Link>
     </div>
   </div>
-);
+)
+
+
+
+// state = {
+//   search: '',
+//   menuIsOpen: false
+// }
+
+// handleClick = () => {
+//   this.setState({
+//     menuIsOpen: !this.state.menuIsOpen
+//   });
+// };
+// {
+//   this.props.status.userIsSignedIn &&
+//   <Fab>
+//     <List
+//       component="nav"
+//       aria-labelledby="nested-list-subheader"
+//     >
+//       <ListItem button onClick={this.handleClick}>
+//         {/* <ListItemIcon> */}
+//                 Profile
+//               {/* </ListItemIcon> */}
+//         {this.state.menuIsOpen ? <ExpandLess /> : <ExpandMore />}
+//       </ListItem>
+//       <Collapse in={this.state.menuIsOpen} timeout="auto" unmountOnExit>
+//         <List component="div">
+//           <ListItem button onClick={this.handleClick}>
+//             {/* <ListItemIcon>
+//                     <StarBorder />
+//                   </ListItemIcon> */}
+//             {/* <ListItemText primary="Starred" /> */}
+//             {/* Games */}
+//             <ListItemText primary="Games" />
+//           </ListItem >
+//           <ListItem button onClick={this.handleClick}>
+//             <ListItemText primary="Friends" />
+//             {/* Friends */}
+//           </ListItem>
+//           <ListItem button onClick={this.handleClick}>
+//             <SettingsIcon />
+//             <ListItemText primary="Settings" />
+//             {/* Settings */}
+//           </ListItem>
+//         </List>
+//       </Collapse>
+//     </List>
+//   </Fab>
+// }
 
 // Instead of taking everything from state, we just want the user
 // object to determine if they are logged in
@@ -39,7 +111,8 @@ const Nav = (props) => (
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({ user }) => ({ user });
 const mapStateToProps = state => ({
-  user: state.user
+  user: state.user,
+  status: state.status
 });
 
 export default connect(mapStateToProps)(Nav);
