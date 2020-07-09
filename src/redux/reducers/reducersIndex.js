ccee58eaed2863d614102eb6af736fbe44ae70ec
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-09 13:39:21 -0500

    Remove excess global state.

--- src/redux/reducers/reducersIndex.js
+++ src/redux/reducers/reducersIndex.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import searchBGG from './searchingBBGReducers';
 import status from './statusReducers';
-import games from './gamesReducer';
-import scheduling from './schedulingReducers';
 import errors from './errorsReducer';
 import loginMode from './loginModeReducer';
 import user from './userReducer';
@@ -11,8 +9,6 @@ import searchUsers from './searchUsersReducers';
 export default combineReducers({
   searchBGG,
   status,
-  games,
-  scheduling,
   // contains registrationMessage and loginMessage
   errors,
   // will have a value of 'login' or 'registration' to control which screen is shown
