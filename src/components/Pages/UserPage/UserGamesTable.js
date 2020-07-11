e543f5c78160960c17b3676a29fc543bb0d0e5cf
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-11 14:53:16 -0500

    Adjust preset code.

--- src/components/Pages/UserPage/UserGamesTable.js
+++ src/components/Pages/UserPage/UserGamesTable.js
@@ -1,54 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import SearchTablePresets from '../../Components/GamesTable/GamesTable';
-import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert';
+import MUIDataTable from 'mui-datatables';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 
 const useStyles = createMuiTheme(
   SearchTablePresets.theme
 );
 
-function Alert(props) {
-  return <MuiAlert elevation={6} variant='filled' {...props} />;
-}
-
-class UserGamesTable extends Component {
+class Table extends React.Component {
   render() {
-    // this.props.tableData
+    const baseData = this.props.searchBGG.formattedGameSearchResults.map((gameObj, index) => [
+      <img src={gameObj.artwork} alt={gameObj.title} key={`game-result-artwork-${index}`} />,
+      gameObj.title,
+      <a key={`game-table-row-${index}`} id={gameObj.BGGid} href={`https://boardgamegeek.com/boardgame/${gameObj.BGGid}`}>More Info</a>,
+      gameObj.playerRange,
+      gameObj.playTime
+    ]);
+    let fullData = baseData;
+    const columns = [...SearchTablePresets.columns];
+    if (this.props.userStatus.userIsSignedIn) {
+      columns.unshift(
+        {
+          name: 'Owned',
+          options: {
+            filter: true,
+            customBodyRender: (value = false, tableMeta) => {
+              return (
+                <FormControlLabel
+                  control={
+                    <Checkbox color='primary' checked={value.props.checked} value={value.props.checked} />
+                  }
+                  onClick={() => {
+                    const bodyObj = {
+                      ownedStatus: value.props.checked,
+                      BGGid: tableMeta.rowData[SearchTablePresets.moreInfoColumnIndex].props.id
+                    };
+                    this.props.dispatch({ type: 'UPDATE_USER_OWNED_GAME', payload: bodyObj });
+                  }}
+                />
+              );
+            }
+          }
+        }
+      );
+      fullData = this.props.searchBGG.formattedGameSearchResults.map((gameObj, index) => {
+        return [<Checkbox color='primary' checked={gameObj.owned} key={`game-search-table-row-${index}`} />, ...baseData[index]];
+      });
+    }
+
     return (
-      <>
-        <div>
-          <h1 id='welcome'>
-            Welcome, {this.props.user.userAttributes.user_name} -test!
-          </h1>
-          <p>Your ID is: {this.props.user.userAttributes.user_id} -1</p>
-        </div>
-        <br />
-        <MuiThemeProvider theme={useStyles}>
-          <Snackbar
-            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-            open={this.props.errors.editGamesMessage}
-            autoHideDuration={10000}
-            onClose={() => this.props.dispatch({ type: 'CLEAR_EDIT_GAMES_ERROR' })}
-          >
-            <Alert
-              onClose={() => this.props.dispatch({ type: 'CLEAR_EDIT_GAMES_ERROR' })}
-              severity='error'
-            >
-              {this.props.errors.editGamesMessage}
-            </Alert>
-          </Snackbar>
-        </MuiThemeProvider>
-      </>
+      <MuiThemeProvider theme={useStyles}>
+        <MUIDataTable title='Search Page' data={fullData} columns={columns} options={SearchTablePresets.options} />
+      </MuiThemeProvider>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  user: state.user,
-  errors: state.errors
+const mapStateToProps = reduxState => ({
+  searchBGG: reduxState.searchBGG,
+  userStatus: reduxState.status
 });
 
-// this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(UserGamesTable);
+export default connect(mapStateToProps)(Table);
