b9f4db490106c7cd20b3715eb5b7f4112481ffc2
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-12 13:41:00 -0500

    Display a user's games when viewing their page.

--- src/components/Pages/LoggedOutUserProfilePage/LoggedOutUserPage.js
+++ src/components/Pages/LoggedOutUserProfilePage/LoggedOutUserPage.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import baseGamesDataArray from '../../Components/GamesTable/GamesTableStandardColumns';
+import SearchTablePresets from '../../Components/GamesTable/GamesTable';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+import MUIDataTable from 'mui-datatables';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+const useStyles = createMuiTheme(
+  SearchTablePresets.theme
+);
 
 class UserPage extends Component {
-
   componentDidMount() {
     this.props.dispatch({ type: 'FETCH_A_DIFFERENT_USER', payload: this.props.match.params.userName });
   }
@@ -12,11 +21,65 @@ class UserPage extends Component {
   }
 
   render() {
+    const baseData = baseGamesDataArray(this.props.otherUsersGames);
+    let fullData = baseData;
+    fullData = this.props.otherUsersGames.map((gameObj, index) => {
+      return [...baseData[index], gameObj.comments];
+    });
+    const columns = [...SearchTablePresets.columns];
+    columns.push(
+      {
+        name: 'Comments',
+        options: {
+          filter: true,
+          sort: true
+        }
+      }
+    );
+    if (this.props.userStatus.userIsSignedIn) {
+      columns.unshift(
+        {
+          name: 'Request',
+          options: {
+            filter: false,
+            sort: false,
+            customBodyRender: (value = false, tableMeta) => {
+              return (
+                <FormControlLabel
+                  control={
+                    <Checkbox color='primary' checked={value.props.checked} value={value.props.checked} />
+                  }
+                  onClick={() => {
+                    const bodyObj = {
+                      ownedStatus: value.props.checked,
+                      BGGid: tableMeta.rowData[SearchTablePresets.moreInfoColumnIndex].props.id
+                    };
+                    this.props.dispatch({ type: 'UPDATE_USER_OWNED_GAME', payload: bodyObj });
+                  }}
+                />
+              );
+            }
+          }
+        }
+      );
+      fullData = this.props.otherUsersGames.map((gameObj, index) => {
+        return [<Checkbox color='primary' checked={gameObj.owned} key={`game-search-table-row-${index}`} />, ...fullData[index]];
+      });
+    }
     return (
       <>
         {`This is ${this.props.match.params.userName}'s logged out page`}
+        <MuiThemeProvider theme={useStyles}>
+          <MUIDataTable title='Search Page' data={fullData} columns={columns} options={SearchTablePresets.options} />
+        </MuiThemeProvider>
       </>
     );
   }
 }
-export default connect()(UserPage);
+
+const mapStateToProps = reduxState => ({
+  otherUsersGames: reduxState.loggedOut.otherUsersGames,
+  userStatus: reduxState.status
+});
+
+export default connect(mapStateToProps)(UserPage);
