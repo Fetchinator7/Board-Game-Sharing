f1ddd107e84fc5131f6b65a3d957ac6a4fe3d74d
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-14 15:21:15 -0500

    Remove game confirmation dialogue.

--- src/components/Pages/SearchPageGames/SearchPageGamesTable.js
+++ src/components/Pages/SearchPageGames/SearchPageGamesTable.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ConfirmationDialogue from '../../Components/Notifications/ConfirmationDialogue';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import SearchTablePresets from '../../Components/GamesTable/GamesTable';
@@ -13,6 +14,23 @@ const useStyles = createMuiTheme(
 );
 
 class Table extends React.Component {
+  state = {
+    bodyObj: {},
+    gameTitle: '',
+    showDialogue: false
+  }
+
+  confirmBeforeDeleting = (proceedBool) => {
+    if (proceedBool) {
+      this.props.dispatch({ type: 'UPDATE_USER_OWNED_GAME', payload: this.state.bodyObj });
+    }
+    this.setState({
+      bodyObj: {},
+      gameTitle: '',
+      showDialogue: false
+    })
+  }
+
   render() {
     const data = this.props.searchBGG.rawGameSearchResults;
     const result = [];
@@ -38,11 +56,21 @@ class Table extends React.Component {
                     <Checkbox color='primary' checked={value.props.checked} value={value.props.checked} />
                   }
                   onClick={() => {
+                    const BGGid = tableMeta.rowData[SearchTablePresets.moreInfoColumnIndex].props.id
                     const bodyObj = {
                       ownedStatus: value.props.checked,
-                      BGGid: tableMeta.rowData[SearchTablePresets.moreInfoColumnIndex].props.id
+                      BGGid: BGGid,
+                      gameTitle: tableMeta.rowData[SearchTablePresets.moreInfoColumnIndex].props.id
                     };
-                    this.props.dispatch({ type: 'UPDATE_USER_OWNED_GAME', payload: bodyObj });
+                    if (value.props.checked) {
+                      this.setState({
+                        bodyObj: bodyObj,
+                        gameTitle: tableMeta.rowData[2],
+                        showDialogue: true
+                      })
+                    } else {
+                      this.props.dispatch({ type: 'UPDATE_USER_OWNED_GAME', payload: bodyObj });
+                    }
                   }}
                 />
               );
@@ -52,15 +80,23 @@ class Table extends React.Component {
       );
       fullData = this.props.searchBGG.formattedGameSearchResults.map((gameObj, index) => {
         const owned = this.props.usersGames.some(userGameObj => userGameObj.bgg_game_id === gameObj.bgg_game_id);
-        console.log('owned:', gameObj.title, gameObj.owned, owned);
         return [<Checkbox color='primary' checked={owned} key={`game-search-table-row-${index}`} />, ...baseData[index]];
       });
     }
 
     return (
+      <>
       <MuiThemeProvider theme={useStyles}>
         <MUIDataTable title='Search Page' data={fullData} columns={columns} options={SearchTablePresets.options} />
       </MuiThemeProvider>
+      <ConfirmationDialogue
+        parentCallBackFunc={this.confirmBeforeDeleting}
+        visible={this.state.showDialogue}
+        title={`Are you sure you want to delete your game "${this.state.gameTitle}"?`}
+        trueButtonAction={'Proceed'}
+        showTextField={false}
+      />
+      </>
     );
   }
 }
