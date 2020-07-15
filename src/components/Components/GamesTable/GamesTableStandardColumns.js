4ad9270ab989f3a29a4c2202c206072d34e9c063
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-15 18:49:35 -0500

    Make link a button.

--- src/components/Components/GamesTable/GamesTableStandardColumns.js
+++ src/components/Components/GamesTable/GamesTableStandardColumns.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import OpenInBrowserIcon from '@material-ui/icons/OpenInBrowser';
+import { Button } from '@material-ui/core';
 
 const Table = baseDataArray => {
   const baseData = baseDataArray.map((gameObj, index) => [
     <img src={gameObj.game_img} alt={gameObj.title} key={`game-result-artwork-${index}`} />,
     gameObj.title,
-    <a key={`game-table-row-${index}`} id={gameObj.bgg_game_id} href={`https://boardgamegeek.com/boardgame/${gameObj.bgg_game_id}`}>More Info</a>,
+    <Button
+      key={`game-table-row-${index}`}
+      href={`https://boardgamegeek.com/boardgame/${gameObj.bgg_game_id}`}
+      color='primary'
+      variant='contained'
+      startIcon={<OpenInBrowserIcon />}
+    > More Info
+    </Button>,
     gameObj.player_range,
     gameObj.playtime
   ]);
